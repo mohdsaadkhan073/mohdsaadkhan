4e3ba33769b6872754c7081023a0503ece3824eb
Mohd Saad Khan <mohdsaadkhan073@gmail.com>
2026-08-08 13:24:01 +0530

    Applied /initial things

--- src/App.tsx
+++ src/App.tsx
@@ -40,7 +40,7 @@ const App = () => {
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename="/initial">
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/projects" element={<ProjectsPage />} />
